d0b396abcf61c82f17fef409ace29ea26a7c2102
D-M4N <56065975+D-M4N@users.noreply.github.com>
2024-02-26 17:47:56 +0200

    remove

--- Chat/index.js
+++ Chat/index.js
@@ -9,15 +9,6 @@ var firebaseConfig = {
   measurementId: "G-BB1WMBCHER"
 };
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 
