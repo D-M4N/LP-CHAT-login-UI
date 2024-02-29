fde0ff83ef0c419577795d0546c0654b5be7879c
D-M4N <56065975+D-M4N@users.noreply.github.com>
2024-02-29 10:22:02 +0200

    Update MainActivity.java

--- Chat/MainActivity.java
+++ Chat/MainActivity.java
@@ -20,3 +20,38 @@ public class MyActivity extends AppCompatActivity {
       .setValue("Hello, Firebase!");
   }
 }
+
+//You can listen for changes in the database using a ValueEventListener:
+
+import com.google.firebase.database.ValueEventListener;
+
+public class MyActivity extends AppCompatActivity {
+  private ValueEventListener mValueEventListener;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    FirebaseDatabase.getInstance().getReference("mydata")
+      .addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+          String value = dataSnapshot.getValue(String.class);
+          Log.d("MyActivity", "Value is: " + value);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+          Log.w("MyActivity", "Listen failed: " + databaseError.toException());
+        }
+      });
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (mValueEventListener != null) {
+      FirebaseDatabase.getInstance().getReference("mydata")
+        .removeEventListener(mValueEventListener);
+    }
+  }
+}
